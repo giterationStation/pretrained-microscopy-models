e392b55bce06fc8f02f8b6eb7da4f87d7865f505
Jonathan Keeley <jonathan.keeley@graphenedx.com>
2023-10-05 15:10:15 -0400

    Update

--- pretrained_microscopy_models/losses.py
+++ pretrained_microscopy_models/losses.py
@@ -9,18 +9,26 @@ class DiceBCELoss(nn.Module):
         self.__name__ = 'DiceBCELoss'
 
     def forward(self, inputs, targets, smooth=1):
-        inputs = F.sigmoid(inputs)       
+        inputs = F.sigmoid(inputs)
+        
+        # Convert one-hot encoded masks to class indices
+        _, class_indices = targets.max(dim=1)
+        
+        # Use class indices to get class weights for each pixel
+        weights = self.class_weights[class_indices].float()
+        
+        # Flatten tensors
         inputs_flat = inputs.view(-1)
         targets_flat = targets.view(-1)
+        weights_flat = weights.view(-1)
         
         intersection = (inputs_flat * targets_flat).sum()                            
         dice_loss = 1 - (2.*intersection + smooth)/(inputs_flat.sum() + targets_flat.sum() + smooth)  
         
         # Compute the weighted BCE loss
         if self.class_weights is not None:
-            weights = self.class_weights[targets.long()].view(-1).float()  # Flatten the weights tensor
             BCE = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='none')
-            BCE = (BCE * weights).mean()
+            BCE = (BCE * weights_flat).mean()
         else:
             BCE = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='mean')
         
--- pretrained_microscopy_models/segmentation_training.py
+++ pretrained_microscopy_models/segmentation_training.py
@@ -340,7 +340,7 @@ def train_segmentation_model(model,
     valid_loader = DataLoader(validation_dataset, batch_size=val_batch_size, 
                               shuffle=False, num_workers=num_workers, pin_memory=True)
     
-    class_weights = torch.tensor([0.5, 1.0, 1.0, 1.0, 2.0]).to(device)  # Adjust these values as needed
+    class_weights = torch.tensor([0.01, 1.0, 1.0, 1.0, 100.0]).to(device)  # Adjust these values as needed
     loss = losses.DiceBCELoss(weight=0.7, class_weights=class_weights) if loss is None else loss
 
     metrics = [smp.utils.metrics.IoU(threshold=0.5),]
