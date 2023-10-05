ee6741dbf7d69de8853badf0860702c80c00a8b7
Jonathan Keeley <jonathan.keeley@graphenedx.com>
2023-10-05 15:20:18 -0400

    Update

--- pretrained_microscopy_models/losses.py
+++ pretrained_microscopy_models/losses.py
@@ -8,25 +8,25 @@ class DiceBCELoss(nn.Module):
         self.class_weights = class_weights
         self.__name__ = 'DiceBCELoss'
 
-def forward(self, inputs, targets, smooth=1):
-    inputs = F.sigmoid(inputs)
-    inputs_flat = inputs.view(-1)
-    targets_flat = targets.view(-1)
-    
-    # Calculate dice loss
-    intersection = (inputs_flat * targets_flat).sum()
-    dice_loss = 1 - (2.*intersection + smooth) / (inputs_flat.sum() + targets_flat.sum() + smooth)
-    
-    # Calculate BCE loss with weights
-    if self.class_weights is not None:
-        # Expand class_weights to match the shape of inputs and then flatten
-        weights = self.class_weights[targets.long()]
-        weights_flat = weights.view(-1)
-        BCE = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='none')
-        BCE = (BCE * weights_flat).mean()
-    else:
-        BCE = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='mean')
-    
-    # Combine BCE and dice loss
-    Dice_BCE = self.weight * BCE + (1-self.weight) * dice_loss
-    return Dice_BCE
+    def forward(self, inputs, targets, smooth=1):
+        inputs = F.sigmoid(inputs)
+        inputs_flat = inputs.view(-1)
+        targets_flat = targets.view(-1)
+        
+        # Calculate dice loss
+        intersection = (inputs_flat * targets_flat).sum()
+        dice_loss = 1 - (2.*intersection + smooth) / (inputs_flat.sum() + targets_flat.sum() + smooth)
+        
+        # Calculate BCE loss with weights
+        if self.class_weights is not None:
+            # Expand class_weights to match the shape of inputs and then flatten
+            weights = self.class_weights[targets.long()]
+            weights_flat = weights.view(-1)
+            BCE = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='none')
+            BCE = (BCE * weights_flat).mean()
+        else:
+            BCE = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='mean')
+        
+        # Combine BCE and dice loss
+        Dice_BCE = self.weight * BCE + (1-self.weight) * dice_loss
+        return Dice_BCE
