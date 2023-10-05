9caba33569bcdf4a267836976d5089e00ce225ab
Jonathan Keeley <jonathan.keeley@graphenedx.com>
2023-10-05 14:28:44 -0400

    Update

--- pretrained_microscopy_models/losses.py
+++ pretrained_microscopy_models/losses.py
@@ -2,9 +2,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DiceBCELoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
+    def __init__(self, weight=None, class_weights=None, size_average=True):
         super(DiceBCELoss, self).__init__()
         self.weight = weight
+        self.class_weights = class_weights
         self.__name__ = 'DiceBCELoss'
 
     def forward(self, inputs, targets, smooth=1):
@@ -12,21 +13,18 @@ class DiceBCELoss(nn.Module):
         # Comment out if your model contains a sigmoid or equivalent activation layer
         inputs = F.sigmoid(inputs)       
         
-        # Number of classes
-        num_classes = inputs.shape[1]
+        # Flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
         
-        # List to store per-class losses
-        per_class_losses = []
+        # Compute the Dice Loss
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
         
-        for cls in range(num_classes):
-            input_cls = inputs[:, cls, ...].contiguous().view(-1)
-            target_cls = targets[:, cls, ...].contiguous().view(-1)
-            
-            intersection = (input_cls * target_cls).sum()                            
-            dice_loss = 1 - (2.*intersection + smooth) / (input_cls.sum() + target_cls.sum() + smooth)
-            BCE = F.binary_cross_entropy(input_cls, target_cls, reduction='mean')
-            Dice_BCE = self.weight * BCE + (1-self.weight) * dice_loss
-            
-            per_class_losses.append(Dice_BCE)
+        # Compute the weighted BCE
+        BCE = F.binary_cross_entropy(inputs, targets, weight=self.class_weights, reduction='mean')
         
-        return per_class_losses
+        # Combine the Dice and BCE losses
+        Dice_BCE = self.weight * BCE + (1-self.weight) * dice_loss
+        
+        return Dice_BCE
